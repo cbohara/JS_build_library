d758156164dfd9f27b85c418133caf87dc599fad
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-02-10 10:39:47 -0800

    completed allPass!

--- myFirstLibrary.js
+++ myFirstLibrary.js
@@ -1,78 +1,112 @@
-  
+     /* 
+    1. loopThrough takes an array and a callback and invokes the callback on each value inside of the array.
+    var print = function(val){
+      console.log(val);
+    };
+    var truth = "Prep+ is the best".split(' ');
+    */
 //loopThrough takes an array and a callback and invokes the callback on each value inside of the array
-  window.loopThrough = function(array, callback){
-    for(var i = 0; i < array.length; i++){
+window.loopThrough = function(array, callback){
+  for(var i = 0; i < array.length; i++){
       callback(array[i]);
-    }
-  };  
+  }
+};  
+// loopThrough(truth, print);
 
+    /*
+    1. copyBy takes an array, and a callback, and returns a new array containing the result of the callback invoked on each value.
+    2. Requirement: copyBy must utilize loopThrough to go through the array and perform the associated functionality.
+    example:
+    */
+   /*var nums = [1, 2, 3, 4, 5];
+    var mult2 = function(val){
+      return val * 2;
+    };
+    */
+    /*
+    var truth = ['I', "love", "functional", "programming"];
+    var upperCase = function(val){
+      return val.toUpperCase()
+    };
+    */
 // copyBy takes an array, and a callback, and returns a new array containing the result of the callback invoked on each value
-  window.copyBy = function(array, callback){
+window.copyBy = function(array, callback){
   // new array to be returned
   var result = [];
-
   // utilize loopThrough function and push new values into the results array
   loopThrough(array,function(val) {result.push(callback(val));} );
-
   return result;
-  };  
+}; 
 
-  // filterBy takes an array, and a predicate, and returns an array filled by with ONLY the values that pass the predicates truth test
-  window.filterBy = function(array, predicate){
-    var result = [];
-    
-    loopThrough(array, function(val) {result.push(predicate(val));} );
+// copyBy(nums, mult2);
+// copyBy(truth, upperCase);
 
-    return result;
-  };
-
-
- var nums = [1, 2, 3, 4, 6];
-    
-    var isEven = function(val){
-      return val % 2 === 0;
-    };
-
-var evenArray = [2, 4, 6, 8, 10];
-
-
-// allPass takes an array and a predicate and returns true if every value in the array passes the truth test, and returns false if even one of the fails the predicate test
-  window.allPass = function(array, predicate){
-      loopThrough(array, function(val) {
-        // if element in array is true
-        if(val === false){
-          return false;
-        }
-        else{
-          return true;
-        }
-  });
-};
-
-    var test = allPass(nums, isEven);
-    console.log(test);
     /*
-    1. allPass takes an array and a predicate and returns true if every value in the array passes the truth test, and returns false if even one of the fails the predicate test.
-
-    2. Requirement: allPass must utilize loopThrough to go through the array and perform the associated functionality.
-
-
-    Example:
-
     var nums = [1, 2, 3, 4, 6];
     
     var isEven = function(val){
-      return val % 2 === 0;
+      if(val % 2 === 0){
+        return true;
+      }
+      else{
+        return false;
+      }
     };
-    
-    allPass(nums, isEven) --> false
+    */
+// filterBy takes an array, and a predicate, and returns an array filled by with ONLY the values that pass the predicates truth test
+window.filterBy = function(array, predicate){
+  // new array to be returned
+  var result = [];
+  // utilize loopThrough function and push values that pass the predicate test into the results array
+  loopThrough(array, function(val) {result.push(predicate(val));});
+  return result;
+};
 
-    var evenArray = [2, 4, 6, 8, 10];
+// filterBy(nums,isEven);
 
-    allPass(evenArray, isEven) --> true;
+
+var nums = [1, 2, 3, 4, 6];
+
+var evenArray = [2, 4, 6, 8, 10];
     
-  */
-  
+var isEven = function(val){
+  return val % 2 === 0;
+};
+
+// allPass takes an array and a predicate and returns true if every value in the array passes the predicate test. returns false if any value in array fails the predicate test. 
+
+var filterFalse = function(val){      
+  return val === false;
+};
+
+window.allPass = function(array, predicate){
+  // create array to hold result values
+  var predicateResult = [];
+  // loop though the array and push the boolean value result of the predicate test into the result array
+  loopThrough(array, function(val){predicateResult.push(predicate(val));});
+  // filter through predicateResult array to return a new array of false values, if they exist 
+  var filterForFalseArray = predicateResult.filter(filterFalse);
+  // if filterForFalseArray contains any values, then the predicate returned false for at least one value in the original array. therefore return false. otherwise return true.
+  if(filterForFalseArray.length > 0){
+    return false;
+  }
+  else{
+    return true;
+  }
+};
+
+// allPass(nums, isEven);
+// allPass(evenArray, isEven);    
+
+
+
+
+
+
+
+
+
+
 
   window.somePass = function(array, callback){
 
